368663107df870e53aa02fdaae81d2f16b72a010
Sergio Aragonés <sergioat7@gmail.com>
2020-10-21 21:25:42 +0200

    Created project

---
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by alumno on 21/10/2020.
+//  Copyright © 2020 alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
