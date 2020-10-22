5292d102bf56829921fedf7565d178225659d0ac
Sergio Aragonés <sergioat7@gmail.com>
2020-10-22 13:50:32 +0200

    Created request/response files for GET search photos

---
+++ ImageGallery/ImageGallery/Code/API/Requests/Images/Model/SearchResponse.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResponse.swift
+//  ImageGallery
+//
+//  Created by Sergio Aragonés on 22/10/2020.
+//  Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResponse: Codable {
+    let photos: SearchPhotosResponse
+}
