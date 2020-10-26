67bf40de31b4197ff1e21bbec3e4459a125ca46c
Sergio Aragonés <sergioat7@gmail.com>
2020-10-26 14:04:29 +0100

    Code refactoring

--- ImageGallery/ImageGallery/Code/Controllers/PhotoGrid/PhotoGridViewModel.swift
+++ ImageGallery/ImageGallery/Code/Controllers/PhotoGrid/PhotoGridViewModel.swift
@@ -71,7 +71,7 @@ class PhotoGridViewModel: BaseViewModel {
                                       success: { photoSizesResponse in
                                         
                                         let imageUrl = photoSizesResponse.first(where: { $0.label.elementsEqual("Large Square") })?.source
-                                        let fullImageUrl = photoSizesResponse.first(where: { $0.label.elementsEqual("Large") })?.source
+                                        let fullImageUrl = self.getFullImageUrl(sizes: photoSizesResponse)
                                         photoCellViewModels.append(PhotoCellViewModel(photo: photoResponse,
                                                                                       image: imageUrl,
                                                                                       fullImage: fullImageUrl))
@@ -87,6 +87,21 @@ class PhotoGridViewModel: BaseViewModel {
                                             success(photoCellViewModels)
                                         }
                                       })
+        }
+    }
+    
+    private func getFullImageUrl(sizes: PhotoSizesResponse) -> String? {
+        
+        if let large = sizes.first(where: { $0.label.elementsEqual("Large") })?.source {
+            return large
+        } else if let large = sizes.first(where: { $0.label.elementsEqual("Original") })?.source {
+            return large
+        } else if let large2048 = sizes.first(where: { $0.label.elementsEqual("Large 2048") })?.source {
+            return large2048
+        } else if let large1600 = sizes.first(where: { $0.label.elementsEqual("Large 1600") })?.source {
+            return large1600
+        } else {
+            return nil
         }
     }
 }
